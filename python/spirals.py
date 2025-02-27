9e8e9db0e5aa65ede125b2398c55fb7b39999a52
David Jourdan <djourdan@berkeley.edu>
2025-02-26 19:15:01 -0800

    spiral code

---
+++ python/spirals.py
@@ -0,0 +1,106 @@
+import shrink_morph_py
+import numpy as np
+import polyscope as ps
+import polyscope.imgui as gui
+import togcode
+from tkinter import filedialog
+filename = filedialog.asksaveasfilename(defaultextension='.gcode')
+
+def display_trajectories(nodes, edges):
+  ps_traj = ps.register_curve_network("Layer", nodes, edges, enabled=True, radius=printer.nozzle_width / 2)
+  ps_traj.set_radius(printer.nozzle_width / 2, relative=False)
+  ps_traj.set_color((0.3, 0.6, 0.8))
+
+
+def convert_trajectories(trajectories):
+  nodes = trajectories[0]
+  edges = np.empty([nodes.shape[0] - 1, 2])
+  edges[:, 0] = np.arange(nodes.shape[0] - 1)
+  edges[:, 1] = np.arange(1, nodes.shape[0])
+
+  for traj in trajectories:
+    new_edges = np.empty([traj.shape[0] - 1, 2])
+    new_edges[:, 0] = np.arange(nodes.shape[0], nodes.shape[0] + traj.shape[0] - 1)
+    new_edges[:, 1] = np.arange(nodes.shape[0] + 1, nodes.shape[0] + traj.shape[0])
+    
+    nodes = np.vstack((nodes, traj))
+    edges = np.vstack((edges, new_edges))
+
+  return nodes, edges
+
+printer_profile = "Prusa_MK3S"
+printer = togcode.Printer(printer_profile)
+printer.layer_height = 0.2
+
+lambda1 = 1
+lambda2 = 0.98
+radius = 20
+thickness = 0.4
+angle = 0
+
+P, F = shrink_morph_py.read_from_OBJ("data/hemisphere.obj")
+P[:,1] = P[:,2].copy()
+P[:,2] *= 0
+P *= radius / (np.max(P) - np.min(P))
+
+
+ps.set_give_focus_on_show(True)
+ps.init()
+ps.set_ground_plane_mode("shadow_only")
+ps.register_surface_mesh("Parameterization", P, F, edge_width=1, color=(42/255, 53/255, 213/255))
+
+
+theta = np.empty(P.shape[0])
+def callback():
+  global thickness, radius, angle, lambda2, P, trajectories, printer
+
+  gui.PushItemWidth(50)
+  _, printer.nozzle_width = gui.InputDouble("Nozzle width (mm)", printer.nozzle_width, format="%.2f")    
+  _, printer.print_speed = gui.InputDouble("Printing speed (mm/s)", printer.print_speed, format="%.0f")    
+  _, printer.first_layer_speed = gui.InputDouble("First layer speed (mm/s)", printer.first_layer_speed, format="%.0f")
+  _, thickness = gui.InputDouble("Total thickness", thickness, format="%.2f")
+  _, printer.layer_height = gui.InputDouble("Layer height", printer.layer_height, format="%.2f")
+  _, printer.bed_temp = gui.InputDouble("Bed temperature (ºC)", printer.bed_temp, format="%.0f")
+  _, printer.extruder_temp = gui.InputDouble("Nozzle temperature (ºC)", printer.extruder_temp, format="%.0f")
+  _, lambda2 = gui.InputDouble("Shrinking ratio", lambda2, format="%.2f")
+  changed, radius = gui.DragFloat("Radius (mm)", radius, 1, 1, 100, "%.0f")
+  if changed and radius > 0:
+    P *= radius / (np.max(P) - np.min(P))
+    ps.get_surface_mesh("Parameterization").update_vertex_positions(P)
+  _, angle = gui.DragFloat("Spiral pitch angle (degrees)", angle, 1, 0, 360, "%.2f")
+
+  if gui.Button("Generate spiral pattern"):
+    theta = np.arctan2(P[:, 1], P[:, 0])
+    theta += np.radians(angle)
+
+    stripe = shrink_morph_py.StripeAlgo(P[:, :2], F)
+    layer = stripe.generate_one_layer(P[:, :2], F, theta, 0 * theta, printer.layer_height, printer.nozzle_width, 10, 0)
+    nodes, edges = convert_trajectories(layer)
+    display_trajectories(nodes, edges)
+
+    n_layers = round(thickness / printer.layer_height)
+    trajectories = []
+    for i in range(n_layers):
+      curr_layer = []
+      for j in range(len(layer)):
+        # rotate wrt previous layer
+        s = printer.nozzle_width / radius
+        c = 1 - (printer.nozzle_width / radius)**2 / 2
+        rotation_matrix = np.array([[c, -s], [s, c]])
+        res = rotation_matrix.dot(layer[j].T)
+        layer[j] = res.T
+
+        curr_layer.append(np.column_stack((layer[j], (i + 1) * printer.layer_height * np.ones(layer[j].shape[0]))))
+      trajectories.append({"height": printer.layer_height, "paths": curr_layer})
+
+
+  if gui.Button("Export gcode"):
+    filename = filedialog.asksaveasfilename(defaultextension='.gcode')
+    printer.to_gcode(trajectories, filename, variable_layer_height=True)
+
+
+
+ps.set_user_callback(callback)
+
+
+ps.show()
